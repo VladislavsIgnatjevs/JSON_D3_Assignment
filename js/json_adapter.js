9590474925823c8fbc86fdd44d754c98ea7b4195
BC <vignatjevs@BCs-MacBook-Pro.local>
2016-10-06 16:52:42 +0100

    minor refactoring

--- js/json_adapter.js
+++ js/json_adapter.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     var requestButton = $("#button");
-
-
     requestButton.click(function () {
         get();
     });
@@ -12,30 +10,30 @@ function get() {
     var cityTag = $("#numberOfCities"),
         andyAPI = "http://ac32007.cloudapp.net:8080/Circles/Towns/" + cityTag.val();
 
-
     //get JSON and push into array cities
     $.getJSON(andyAPI, function (data) {
         var cities = [];
         $.each(data, function (key, val) {
-            // items.push( "<li id='" + JSON.stringify(key) + "'>" + JSON.stringify(val) + "</li>" );
+
             cities.push(val);
         });
 
-        //print city attributes
+        //JSON object attributes
         // Town
         // Country
         // Population
-        //lat
-        //lng
-
+        // lat
+        // lng
         $.each(cities, function( key, value ) {
             alert( JSON.stringify(key) + ": " + JSON.stringify(value));
         });
 
 
-        /*        $( "<ul/>", {
-         "class": "my-new-list",
-         html: items.join( "" )
-         }).appendTo( "body" );*/
+        //temp push into the mainpage
+        // items.push( "<li id='" + JSON.stringify(key) + "'>" + JSON.stringify(val) + "</li>" );
+        //         $( "<ul/>", {
+        //  "class": "my-new-list",
+        //  html: items.join( "" )
+        //  }).appendTo( "body" );
     });
 }
